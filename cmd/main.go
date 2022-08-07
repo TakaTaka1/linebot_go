b73a8aa29e637207fb220d76ff5da07c5b3164c7
TakaTaka1 <tk.1987.6.9@gmail.com>
2022-08-07 18:59:09 +0900

    apply fmt

--- cmd/main.go
+++ cmd/main.go
@@ -22,52 +22,6 @@ func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
 	return fmt.Sprintf("Hello %s!", name.Name), nil
 }
 
-func LineNotify () {
-	f := "./.env"
-	if _, err := os.Stat(f); err == nil {
-		err_read := godotenv.Load(f)
-		if err_read != nil {
-			log.Fatalf("error: %v", err_read)
-		}
-		fmt.Println(".env is existed")
-	} else {
-		fmt.Println(".env is not existed")
-	}
-
-	accessToken := os.Getenv("LINE_ACCESS_TOKEN_TEST")
-	msg := "テストメッセージ"
-	URL := "https://notify-api.line.me/api/notify"
-
-	u, err := url.ParseRequestURI(URL)
-	if err != nil {
-		fmt.Println("send error")
-		log.Fatal(err)
-	}
-
-	c := &http.Client{}
-
-	form := url.Values{}
-	form.Add("message", msg)
-
-	body := strings.NewReader(form.Encode())
-
-	req, err := http.NewRequest("POST", u.String(), body)
-	if err != nil {
-		fmt.Println("send error")
-		log.Fatal(err)
-	}
-
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Authorization", "Bearer "+accessToken)
-
-	res, err := c.Do(req)
-	if err != nil {
-		fmt.Println("send error")
-		log.Fatal(err)
-	}
-	fmt.Println(res)
-}
-
 func main() {
 	f := "./.env"
 	if _, err := os.Stat(f); err == nil {
@@ -112,8 +66,7 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(res)
-	
+
 	log.Printf("lambda started!")
 	lambda.Start(HandleRequest)
-	// lambda.Start(LineNotify)
 }
