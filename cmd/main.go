ba519a2e56fde1304f1c782bf89078578b1feeb6
TakaTaka1 <tk.1987.6.9@gmail.com>
2022-08-07 18:31:35 +0900

    fix actions

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"net/url"
+	"os"
 	"strings"
-	"context"
-	
+
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/joho/godotenv"
 )
 
 type MyEvent struct {
-    Name string `json:"Name"`
+	Name string `json:"Name"`
 }
 
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
@@ -22,7 +22,7 @@ func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
 	return fmt.Sprintf("Hello %s!", name.Name), nil
 }
 
-func main() {
+func LineNotify () {
 	f := "./.env"
 	if _, err := os.Stat(f); err == nil {
 		err_read := godotenv.Load(f)
@@ -32,10 +32,10 @@ func main() {
 		fmt.Println(".env is existed")
 	} else {
 		fmt.Println(".env is not existed")
-	}	
+	}
 
 	accessToken := os.Getenv("LINE_ACCESS_TOKEN_TEST")
-	msg := "テストメッセージ"	
+	msg := "テストメッセージ"
 	URL := "https://notify-api.line.me/api/notify"
 
 	u, err := url.ParseRequestURI(URL)
@@ -66,7 +66,54 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(res)
+}
+
+func main() {
+	// f := "./.env"
+	// if _, err := os.Stat(f); err == nil {
+	// 	err_read := godotenv.Load(f)
+	// 	if err_read != nil {
+	// 		log.Fatalf("error: %v", err_read)
+	// 	}
+	// 	fmt.Println(".env is existed")
+	// } else {
+	// 	fmt.Println(".env is not existed")
+	// }
+
+	// accessToken := os.Getenv("LINE_ACCESS_TOKEN_TEST")
+	// msg := "テストメッセージ"
+	// URL := "https://notify-api.line.me/api/notify"
+
+	// u, err := url.ParseRequestURI(URL)
+	// if err != nil {
+	// 	fmt.Println("send error")
+	// 	log.Fatal(err)
+	// }
+
+	// c := &http.Client{}
+
+	// form := url.Values{}
+	// form.Add("message", msg)
+
+	// body := strings.NewReader(form.Encode())
+
+	// req, err := http.NewRequest("POST", u.String(), body)
+	// if err != nil {
+	// 	fmt.Println("send error")
+	// 	log.Fatal(err)
+	// }
+
+	// req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	// req.Header.Set("Authorization", "Bearer "+accessToken)
+
+	// res, err := c.Do(req)
+	// if err != nil {
+	// 	fmt.Println("send error")
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(res)
 
 	log.Printf("lambda started!")
-    lambda.Start(HandleRequest)
+	lambda.Start(HandleRequest)
+	lambda.Start(LineNotify)
 }
