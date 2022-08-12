d9d5fbbb0563cee5d30de0bdb5927f6d301d3938
TakaTaka1 <tk.1987.6.9@gmail.com>
2022-08-12 22:53:15 +0900

    add weedDay

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,24 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"net/url"
 	"os"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/joho/godotenv"
 )
+
+func GetWeekNumber(year, month, day int) int {
+	CurrentDate := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.Local)
+	weekNum := float64(CurrentDate.Day()) / 7
+
+	return int(math.Ceil(weekNum))
+}
 
 type MyEvent struct {
 	Name string `json:"Name"`
@@ -38,8 +47,59 @@ func main() {
 	if !matchedLocal {
 		accessToken = os.Getenv("LINE_ACCESS_TOKEN")
 	}
+	// ゴミの日のポストメッセージ
+	// ゴミ出し日の前日9pmと当日の7:30amにポストする
+	// TODO 曜日判定メソッドのリファクタリング
+	const (
+		Sunday    = 0
+		Monday    = 1
+		Tuesday   = 2
+		Wednesday = 3
+		Thursday  = 4
+		Friday    = 5
+		Saturday  = 6
+		Kanen     = "🔥可燃ゴミ🔥"
+		Funen     = "👀不燃ゴミ👀"
+		Shigen    = "♻️資源ゴミ️️️️️️♻️"
+	)
 
-	msg := "テストメッセージ"
+	today := time.Now()          // 現在日時の取得
+	DayOfWeek := today.Weekday() // 曜日の取得
+	msg := ""
+
+	if DayOfWeek == Tuesday {
+		// 可燃
+		msg += "\n明日は" + Kanen + "収集日です。"
+	}
+	wN := GetWeekNumber(today.Year(), int(today.Month()), today.Day())
+
+	if DayOfWeek == Wednesday {
+		msg += "\n今日は" + Kanen + "収集日です。"
+		if wN == 1 || wN == 3 {
+			msg += "\n明日は" + Funen + "収集日です。"
+		}
+	}
+
+	if DayOfWeek == Thursday && (wN == 1 || wN == 3) {
+		msg += "\n今日は" + Funen + "収集日です。"
+	}
+
+	if DayOfWeek == Thursday {
+		// 資源
+		msg += "\n明日は" + Shigen + "収集日です。"
+	}
+
+	if DayOfWeek == Friday {
+		// 資源・可燃ゴミ
+		msg += "\n今日は" + Shigen + "収集日です。"
+		msg += "\n明日は" + Kanen + "収集日です。"
+	}
+
+	if DayOfWeek == Saturday {
+		// 資源・可燃ゴミ
+		msg += "\n今日は" + Kanen + "収集日です。"
+	}
+
 	URL := "https://notify-api.line.me/api/notify"
 	u, err := url.ParseRequestURI(URL)
 	if err != nil {
@@ -50,6 +110,9 @@ func main() {
 	c := &http.Client{}
 
 	form := url.Values{}
+	if msg == "" {
+		os.Exit(0)
+	}
 	form.Add("message", msg)
 
 	body := strings.NewReader(form.Encode())
